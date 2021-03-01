723c9417d00d10a865a0fe837dca0c0700f2ccd3
Darren Derks <leederksjr@gmail.com>
2021-03-01 03:19:19 -0700

    finished styling cart

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,17 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 import ShopSearchBar from "./shopSearchBar";
 import ShopProduct from "./shopProduct";
+import ShopCart from "./shopCart";
 
 class Shop extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showCart: true,
+    };
+  }
+
   componentDidMount() {
     const headerLinks = [
       {
@@ -35,6 +44,7 @@ class Shop extends Component {
   };
 
   render() {
+    // return <ShopCart className='shop__cart'/>
     return (
       <div className="shop">
         <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
@@ -43,6 +53,8 @@ class Shop extends Component {
             return <ShopProduct {...product} key={product._id} />;
           })}
         </div>
+        {this.state.showCart ? <ShopCart className="shop__cart" /> : ""}
+
         {/* shop cart button */}
       </div>
     );
